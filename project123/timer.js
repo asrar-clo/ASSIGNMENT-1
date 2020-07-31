56066fc72427a4a85098e755a68a384a87267e77
asrar-clo <62931041+asrar-clo@users.noreply.github.com>
2020-07-31 18:07:32 +0530

    Add files via upload

---
+++ project123/timer.js
@@ -0,0 +1,8 @@
+let start = document.getElementById('Start()');
+let stop =  document.getElementById('Stop()');
+let reset = document.getElementById('Reset');
+
+
+start().addEventListener('click',function(){
+   let  start = new date ();
+})
